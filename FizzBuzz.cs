afe3db3249a662c728b8334d9e171a4d9fb4a8f4
anonymously115 <144945617+anonymously115@users.noreply.github.com>
2023-12-01 15:08:20 +0900

    Create FizzBuzz.cs

---
+++ FizzBuzz.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FizzBuzz : MonoBehaviour
+{
+	[SerializeField] InputField number;
+	[SerializeField] InputField fizz;
+	[SerializeField] InputField buzz;
+	[SerializeField] Text fizzbuzz;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	public void OnClick()
+	{
+		int n=int.Parse(number.text);
+		int a=int.Parse(fizz.text);
+		int b=int.Parse(buzz.text);
+		fizzbuzz.text="";
+		for(int i=1;i<=n;i++){
+			if(i%a==0 && i%b==0){
+				fizzbuzz.text+="FizzBuzz";
+			}else if(i%a==0){
+				fizzbuzz.text+="Fizz";
+			}else if(i%b==0){
+				fizzbuzz.text+="Buzz";
+			}else{
+				fizzbuzz.text+=i.ToString();
+			}
+			fizzbuzz.text+="\n";
+		}
+	}
+}
