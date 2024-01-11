57241ec4fb0c69bcf6932c84276502d9c8ed3a37
anonymously115 <AND12915@nifty.com>
2024-01-11 21:07:37 +0900

    PascalCase

--- map.c
+++ map.c
@@ -3,67 +3,98 @@
 #include <stdbool.h>
 #include "map.h"
 
-typedef struct NODE{
+typedef struct node{
 	char* key; //キー
 	void* value; //値
-	struct NODE* child[2]; //child[0]:左の子(キーが親より小さい)、child[1]:右の子(キーが親より大きい)
-}NODE;
+	struct node* child[2]; //child[0]:左の子(キーが親より小さい)、child[1]:右の子(キーが親より大きい)
+}Node;
 
-struct MAP{
-	NODE* root; //根ノード
+struct map{
+	Node* root; //根ノード
 };
 
 /*
  * コンストラクタ
  */
-MAP newMap(){
-	MAP map=(MAP)malloc(sizeof(struct MAP));
-	if(!map){
-		abort();
-	}
-	map->root=NULL;
-	return map;
+Map newMap(){
+    //メモリ確保
+    Map map=(Map)malloc(sizeof(struct map));
+    if(!map){//メモリ確保失敗
+        abort();
+    }
+    //根ノードをNULLにセット
+    map->root=NULL;
+
+    return map;
 }
 
-int height(NODE* node){
-	if(!node){
+/*
+ * 部分木としての高さ
+ */
+int height(Node* node){
+	if(!node){//NULLなら0を返す
 		return 0;
 	}
+
+	//再帰的に、左部分木と右部分木の高さを取得する。
 	int left=height(node->child[0]);
 	int right=height(node->child[1]);
+
+	//左部分木と右部分木の高い方の高さ+1を返す
 	return (left>right?left:right)+1;
 }
 
-int balance(NODE* node){
-	if(!node){
-		abort();
-	}
+/*
+ * 平衡係数。
+ * 今回は、左部分木の高さ－右部分木の高さで定義する。
+ */
+int balance(Node* node){
 	return height(node->child[0])-height(node->child[1]);
 }
 
-void rotate(NODE** node, int flag){//左回転=0, 右回転=1
-	NODE* pivot=(*node)->child[1-flag];
+/*
+ * 部分木の「回転」
+ * node: 付け替え対象となるノードの親ノード
+ * flag: 左回転=0, 右回転=1
+ */
+void rotate(Node** node, int flag){
+	Node* pivot=(*node)->child[1-flag];
 	(*node)->child[1-flag]=pivot->child[flag];
 	pivot->child[flag]=*node;
 	*node=pivot;
 }
 
-void take_balance(MAP map, int depth, NODE* nodes[], bool flag[]){
+/*
+ * 連想配列を構成する二分木の平衡性を保つ処理
+ * map: 連想配列
+ * depth: 挿入ノードまたは削除ノードの深さ
+ * nodes: 根ノードから挿入または削除ノードまで辿ったノード
+ * （nodes[0]が根ノード、nodes[depth]が挿入されたノードまたは削除されたノード※削除された場合は既にNULLになっている）
+ * flag: node[i+1](子)がnode[i](親)に対して左にあるか右にあるか
+ * （flag[i]=0ならnode[i+1]はnode[i]の左の子、flag[i]=1ならnode[i+1]はnode[i]の右の子）
+ */
+void take_balance(Map map, int depth, Node* nodes[], bool flag[]){
+    //対象ノードの親から根に向かって進む
 	for(int i=depth-1;i>=0;i--){
-		int bal=balance(nodes[i]);
-		if(bal>1){
-			if(balance(nodes[i]->child[0])<0){
+        //平衡係数（左部分木の高さ－右部分木の高さ）
+        int bal=balance(nodes[i]);
+		if(bal>1){//左部分木の高さ－右部分木の高さ≧２の場合（平衡性が崩れているので修正）
+			if(balance(nodes[i]->child[0])<0){//左の子について、右部分木の高さが左のそれより高い場合
+                //左の子をつけかえる
 				rotate(&(nodes[i]->child[0]),0);
 			}
+            //平衡係数を計算したノードの親に対する子ノードを繋ぎかえる
 			if(i){
 				rotate(&(nodes[i-1]->child[flag[i-1]]),1);
 			}else{
 				rotate(&(map->root),1);
 			}
-		}else if(bal<-1){
-			if(balance(nodes[i]->child[1])>0){
+		}else if(bal<-1){//右部分木の高さ－左部分木の高さ≧２の場合（平衡性が崩れているので修正）
+			if(balance(nodes[i]->child[1])>0){//右の子について、左部分木の高さが右のそれより高い場合
+                //右の子をつけかえる
 				rotate(&(nodes[i]->child[1]),1);
 			}
+            //平衡係数を計算したノードの親に対する子ノードを繋ぎかえる
 			if(i){
 				rotate(&(nodes[i-1]->child[flag[i-1]]),0);
 			}else{
@@ -73,36 +104,52 @@ void take_balance(MAP map, int depth, NODE* nodes[], bool flag[]){
 	}
 }
 
-void setValue(MAP map, const char* key, void* value){
-	NODE* nodes[64];
+/*
+ * キーと値をセットする
+ */
+void setValue(Map map, const char* key, void* value){
+	//辿ったノードを格納しておくための配列
+	Node* nodes[64];
+
+	//辿ったノードの左右を格納しておくための配列(左がfalse、右がtrue)
 	bool flag[64];
+
+	//辿ったノードの深さ
 	int depth=0;
+
+	//0番目に根ノードを格納しておく
 	nodes[depth]=map->root;
+
+	//配列にNULLが格納されるまで子ノードを辿る
 	while(nodes[depth]){
 		int cmp=strcmp(key,nodes[depth]->key);
-		if(cmp==0){
-			//update処理
+		if(cmp==0){//一致したキーが存在する場合
+			/* update処理 */
 			nodes[depth]->value=value;
 			return;
 		}
+
+		//辞書順比較。新キーが早ければ0(左)、後ならば1(右)
 		flag[depth]=(cmp>0);
-		nodes[depth+1]=nodes[depth]->child[flag[depth]];//流石にnodes[++depth]=nodes[depth]->child[flag]は乱暴すぎる
+
+		//配列に子ノードを格納する（流石にnodes[++depth]=nodes[depth]->child[flag]は乱暴すぎる）
+		nodes[depth+1]=nodes[depth]->child[flag[depth]];
 		depth++;
 	}
-	
+
 	/* insert処理 */
-	//ノードのメモリを確保
-	NODE* node=(NODE*)malloc(sizeof(NODE));
-	if(!node){
+	//新しいノードのメモリを確保
+	Node* node=(Node*)malloc(sizeof(Node));
+	if(!node){//メモリ確保失敗
 		abort();
 	}
 
 	//乱暴な初期化をしてみる（keyもvalueもchild[i]（i=0,1）もNULL）
-	memset(node,0,sizeof(NODE));
+	memset(node,0,sizeof(Node));
 
 	//キーのメモリ確保
 	node->key=(char*)malloc(strlen(key)+1);
-	if(!node->key){
+	if(!node->key){//メモリ確保失敗
 		abort();
 	}
 
@@ -112,14 +159,17 @@ void setValue(MAP map, const char* key, void* value){
 	//値をセット
 	node->value=value;
 
+	//最初に用意した配列に新ノードを格納（実は、後で使わない）
 	nodes[depth]=node;
 	if(depth){
-		nodes[depth-1]->child[flag[depth-1]]=node;
+		//一つ前のノードが親ノード。親ノードに子ノードを紐づける。左右フラグは先程のwhile文の内部で回している。
+    	nodes[depth-1]->child[flag[depth-1]]=node;
 	}else{
+		//要素数が0だった場合、根ノードにセットする。
 		map->root=node;
 	}
 
-	//一応、これで挿入もしくは更新できたのだが、挿入の場合、AVL木の考え方を用いて二分探索木の平衡性を保つことにする。
+	//一応、これで挿入もしくは更新できたのだが、挿入の場合、二分探索木の平衡性を保つことにする。
 	//（その為に、`nodes`に根から辿っていったノードを格納しておいた）
 	take_balance(map, depth, nodes, flag);
 }
@@ -129,13 +179,14 @@ void setValue(MAP map, const char* key, void* value){
  * map : 連想配列
  * key : 探索キー
  */
-void* getValue(MAP map, const char* key){
-	NODE* node=map->root;
+void* getValue(Map map, const char* key){
+	Node* node=map->root;
 	while(node){
 		int cmp=strcmp(key,node->key);
-		if(cmp==0){
+		if(cmp==0){//キーがヒットした場合、値を返す。
 			return node->value;
 		}
+        //引数キーがノードキーより早ければ左、後ならば右の子を探索していく
 		node=node->child[cmp>0];
 	}
 
@@ -146,41 +197,62 @@ void* getValue(MAP map, const char* key){
 /*
  * ノード内のキー・値に対して通りがけ順（左⇒親⇒右）で処理
  */
-void _foreach(void (*func)(const char* key, const void* value), NODE* node){
-	if(node){
+void _foreach(void (*func)(const char* key, const void* value), Node* node){
+	if(node){//node==NULLの場合はスキップ
+		//左の子に対して処理
 		_foreach(func, node->child[0]);
+
+		//自分自身に対して処理
 		func(node->key, node->value);
+
+		//右の子に対して処理
 		_foreach(func, node->child[1]);
 	}
 }
 
-void foreach(MAP map, void (*func)(const char* key, const void* value)){
+void foreach(Map map, void (*func)(const char* key, const void* value)){
+	//根ノードから再帰的に処理
 	_foreach(func, map->root);
 }
 
 /*
  * 連想配列内のキーを削除
+ * 注意：返却値について、mallocでメモリ確保していた場合、関数呼び出し元で責任をもって解放すること!!
  */
-void* erase(MAP map, const char* key){
-	NODE* nodes[64];
+void* erase(Map map, const char* key){
+	//辿ったノードを格納しておくための配列
+	Node* nodes[64];
+
+	//辿ったノードの左右を格納しておくための配列(左がfalse、右がtrue)
 	bool flag[64];
+
+	//辿ったノードの深さ
 	int depth=0;
+
+	//0番目に根ノードを格納しておく
 	nodes[depth]=map->root;
+
 	while(nodes[depth]){
 		int cmp=strcmp(key,nodes[depth]->key);
-		if(cmp){
+		if(cmp){//キーが一致しなかった場合
+			//辞書順比較。新キーが早ければ0(左)、後ならば1(右)
 			flag[depth]=(cmp>0);
+
+			//配列に子ノードを格納する
 			nodes[depth+1]=nodes[depth]->child[flag[depth]];
 			depth++;
 			continue;
 		}
 
 		//削除対象ノード
-		NODE* node=nodes[depth];
+		Node* node=nodes[depth];
 
-		//値を退避
+		//返却値を退避
 		void* value=node->value;
-		
+
+        //削除ノードの、キーを格納するために確保したメモリを解放する。
+        free(node->key);
+
 		/* delete処理 */
 		if(node->child[0] && node->child[1]){//削除対象ノードの左右の子が存在
 			//削除対象ノードの左の右の右の…右のノード（末端ノード）を取得
@@ -192,9 +264,8 @@ void* erase(MAP map, const char* key){
 				nodes[depth+1]=nodes[depth]->child[1];
 				depth++;
 			}
-			
+
 			//末端ノードのキーと値を削除対象ノードに移す
-			free(node->key);
 			node->key=nodes[depth]->key;
 			node->value=nodes[depth]->value;
 
@@ -205,48 +276,64 @@ void* erase(MAP map, const char* key){
 			free(nodes[depth]);
 		}else{
 			if(node->child[0]){//削除対象ノードの左の子が存在
+				//親が自分に指していたポインタを、自分の左に付け替える
 				if(depth){
 					nodes[depth-1]->child[flag[depth-1]]=node->child[0];
 				}else{
 					map->root=node->child[0];
 				}
-				
 			}else if(node->child[1]){//削除対象ノードの右の子が存在
+				//親が自分に指していたポインタを、自分の右に付け替える
 				if(depth){
 					nodes[depth-1]->child[flag[depth-1]]=node->child[1];
 				}else{
 					map->root=node->child[1];
 				}
 			}else{
+				//親が自分に指していたポインタを、NULLに付け替える
 				if(depth){
 					nodes[depth-1]->child[flag[depth-1]]=NULL;
 				}else{
 					map->root=NULL;
 				}
 			}
-			free(node->key);
+			//削除ノードの解放
 			free(node);
 		}
+
+		//nodes[depth]は解放された領域を指しているので、NULLにする。
 		nodes[depth]=NULL;
 
 		//末端ノードの前から根に向かって平衡性を保つように処理
 		take_balance(map, depth, nodes, flag);
+
+		//対象キーに対応していた値を返す（mallocでメモリ確保した場合、呼び出し元で解放せよ）
 		return value;
 	}
 
+	//キーが存在しなかった場合、NULLを返却する。
 	return NULL;
 }
 
 /*
  * 連想配列のノードを解放
- * 右⇒左⇒親の順で解放
+ * 右⇒左⇒親の順で解放（左⇒右⇒親でも構わない）
  */
-void _clear(NODE* node, int flag){
-	if(node){
+void _clear(Node* node, int flag){
+	if(node){//ノードがNULLでない場合
+		//右を解放する
 		_clear(node->child[1],flag);
+
+		//左を解放する
 		_clear(node->child[0],flag);
+
+		//mallocでメモリ確保した場合、解放
 		if(flag)free(node->value);
+
+		//キーを格納するために確保したメモリを解放
 		free(node->key);
+
+		//ノードを解放
 		free(node);
 	}
 }
@@ -254,7 +341,10 @@ void _clear(NODE* node, int flag){
 /*
  * 連想配列を初期化
  */
-void clear(MAP map, int flag){
+void clear(Map map, int flag){
+	//根ノードから再帰的に解放
 	_clear(map->root,flag);
+
+    //根ノードをNULLにセット
 	map->root=NULL;
 }
--- map.h
+++ map.h
@@ -1,20 +1,25 @@
+#ifndef MAP_H_
+#define MAP_H_
+
 /* 連想配列構造体 */
-typedef struct MAP* MAP;
+typedef struct map* Map;
 
 /* コンストラクタ */
-MAP newMap();
+Map newMap();
 
 /* キーと値をセット（キーが既に存在していればupdate, 無ければinsertの役割） */
-void setValue(MAP map, const char* key, void* value);
+void setValue(Map map, const char* key, void* value);
 
 /* 値を取得 */
-void* getValue(MAP map, const char* key);
+void* getValue(Map map, const char* key);
 
 /* 連想配列内の全てのキー、値に対して処理 */
-void foreach(MAP map, void (*func)(const char* key, const void* value));
+void foreach(Map map, void (*func)(const char* key, const void* value));
 
 /* 連想配列内のキーを削除 */
-void* erase(MAP map, const char* key);
+void* erase(Map map, const char* key);
 
 /* 連想配列を初期化（プログラム終了時に忘れない!!） */
-void clear(MAP map, int flag);
+void clear(Map map, int flag);
+
+#endif /* MAP_H_ */
