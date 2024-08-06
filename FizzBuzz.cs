77c92194596422462222437602276aedb5783741
anonymously115 <AND12915@nifty.com>
2024-08-06 11:46:45 +0900

    Formatter

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -5,40 +5,48 @@ using UnityEngine.UI;
 
 public class FizzBuzz : MonoBehaviour
 {
-	[SerializeField] InputField number;
-	[SerializeField] InputField fizz;
-	[SerializeField] InputField buzz;
-	[SerializeField] Text fizzbuzz;
+    [SerializeField] InputField number;
+    [SerializeField] InputField fizz;
+    [SerializeField] InputField buzz;
+    [SerializeField] Text fizzbuzz;
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		
-	}
+    // Start is called before the first frame update
+    void Start()
+    {
 
-	// Update is called once per frame
-	void Update()
-	{
-		
-	}
+    }
 
-	public void OnClick()
-	{
-		int n=int.Parse(number.text);
-		int a=int.Parse(fizz.text);
-		int b=int.Parse(buzz.text);
-		fizzbuzz.text="";
-		for(int i=1;i<=n;i++){
-			if(i%a==0 && i%b==0){
-				fizzbuzz.text+="FizzBuzz";
-			}else if(i%a==0){
-				fizzbuzz.text+="Fizz";
-			}else if(i%b==0){
-				fizzbuzz.text+="Buzz";
-			}else{
-				fizzbuzz.text+=i.ToString();
-			}
-			fizzbuzz.text+="\n";
-		}
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClick()
+    {
+        int n = int.Parse(number.text);
+        int a = int.Parse(fizz.text);
+        int b = int.Parse(buzz.text);
+        fizzbuzz.text = "";
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % a == 0 && i % b == 0)
+            {
+                fizzbuzz.text += "FizzBuzz";
+            }
+            else if (i % a == 0)
+            {
+                fizzbuzz.text += "Fizz";
+            }
+            else if (i % b == 0)
+            {
+                fizzbuzz.text += "Buzz";
+            }
+            else
+            {
+                fizzbuzz.text += i.ToString();
+            }
+            fizzbuzz.text += "\n";
+        }
+    }
 }
--- poker.py
+++ poker.py
@@ -1,114 +1,126 @@
-JOKER=(0,0)
-CARDS=[JOKER]+[(i%4+1,i//4+1) for i in range(52)]
+JOKER = (0, 0)
+CARDS = [JOKER]+[(i % 4+1, i//4+1) for i in range(52)]
 
-nPatterns=0
-cntFiveOfAKind=0
-cntRoyalStraightFlush=0
-cntStraightFlush=0
-cntFourOfAKind=0
-cntFullHouse=0
-cntFlush=0
-cntStraight=0
-cntThreeOfAKind=0
-cntTwoPair=0
-cntOnePair=0
-cntNoPair=0
+nPatterns = 0
+cntFiveOfAKind = 0
+cntRoyalStraightFlush = 0
+cntStraightFlush = 0
+cntFourOfAKind = 0
+cntFullHouse = 0
+cntFlush = 0
+cntStraight = 0
+cntThreeOfAKind = 0
+cntTwoPair = 0
+cntOnePair = 0
+cntNoPair = 0
+
 
 def isFiveOfAKind(hand):
-    return hand[0]==JOKER and hand[1][1]==hand[2][1]==hand[3][1]==hand[4][1]
+    return hand[0] == JOKER and hand[1][1] == hand[2][1] == hand[3][1] == hand[4][1]
+
 
 def isRoyalStraightFlush(hand):
-    if hand[0]==JOKER:
-        if not hand[1][0]==hand[2][0]==hand[3][0]==hand[4][0]:
+    if hand[0] == JOKER:
+        if not hand[1][0] == hand[2][0] == hand[3][0] == hand[4][0]:
             return False
-        return tuple(hand[i][1] for i in range(1,len(hand))) in [(1,10,11,12),(1,10,11,13),(1,10,12,13),(1,11,12,13),(10,11,12,13)]
+        return tuple(hand[i][1] for i in range(1, len(hand))) in [(1, 10, 11, 12), (1, 10, 11, 13), (1, 10, 12, 13), (1, 11, 12, 13), (10, 11, 12, 13)]
     else:
-        if hand[0][1]!=1:
+        if hand[0][1] != 1:
             return False
-        for i in range(1,len(hand)):
-            if hand[i][0]!=hand[0][0] or hand[i][1]!=9+i:
+        for i in range(1, len(hand)):
+            if hand[i][0] != hand[0][0] or hand[i][1] != 9+i:
                 return False
         return True
+
 
 def isStraightFlush(hand):
-    if hand[0]==JOKER:
-        if not hand[1][0]==hand[2][0]==hand[3][0]==hand[4][0]:
+    if hand[0] == JOKER:
+        if not hand[1][0] == hand[2][0] == hand[3][0] == hand[4][0]:
             return False
-        return tuple(hand[i][1]-hand[1][1] for i in range(2,len(hand))) in [(1,2,3),(1,2,4),(1,3,4),(2,3,4)]
+        return tuple(hand[i][1]-hand[1][1] for i in range(2, len(hand))) in [(1, 2, 3), (1, 2, 4), (1, 3, 4), (2, 3, 4)]
     else:
-        for i in range(1,len(hand)):
-            if hand[i][0]!=hand[0][0] or hand[i][1]!=hand[0][1]+i:
+        for i in range(1, len(hand)):
+            if hand[i][0] != hand[0][0] or hand[i][1] != hand[0][1]+i:
                 return False
         return True
 
+
 def isFourOfAKind(hand):
-    if hand[0]==JOKER:
-        return hand[1][1]==hand[2][1]==hand[3][1]\
-            or hand[2][1]==hand[3][1]==hand[4][1]
+    if hand[0] == JOKER:
+        return hand[1][1] == hand[2][1] == hand[3][1]\
+            or hand[2][1] == hand[3][1] == hand[4][1]
     else:
-        return hand[0][1]==hand[1][1]==hand[2][1]==hand[3][1]\
-            or hand[1][1]==hand[2][1]==hand[3][1]==hand[4][1]
+        return hand[0][1] == hand[1][1] == hand[2][1] == hand[3][1]\
+            or hand[1][1] == hand[2][1] == hand[3][1] == hand[4][1]
+
 
 def isFullHouse(hand):
-    if hand[0]==JOKER:
-        return hand[1][1]==hand[2][1]==hand[3][1]\
-            or hand[2][1]==hand[3][1]==hand[4][1]\
-            or hand[1][1]==hand[2][1] and hand[3][1]==hand[4][1]
+    if hand[0] == JOKER:
+        return hand[1][1] == hand[2][1] == hand[3][1]\
+            or hand[2][1] == hand[3][1] == hand[4][1]\
+            or hand[1][1] == hand[2][1] and hand[3][1] == hand[4][1]
     else:
-        return hand[0][1]==hand[1][1]==hand[2][1] and hand[3][1]==hand[4][1]\
-            or hand[0][1]==hand[1][1] and hand[2][1]==hand[3][1]==hand[4][1]
+        return hand[0][1] == hand[1][1] == hand[2][1] and hand[3][1] == hand[4][1]\
+            or hand[0][1] == hand[1][1] and hand[2][1] == hand[3][1] == hand[4][1]
+
 
 def isFlush(hand):
-    j=1 if hand[0]==JOKER else 0
-    for i in range(j+1,len(hand)):
-        if hand[i][0]!=hand[j][0]:
+    j = 1 if hand[0] == JOKER else 0
+    for i in range(j+1, len(hand)):
+        if hand[i][0] != hand[j][0]:
             return False
     return True
 
+
 def isRoyalStraight(hand):
-    if hand[0]==JOKER:
-        return tuple(hand[i][1] for i in range(1,len(hand))) in [(1,10,11,12),(1,10,11,13),(1,10,12,13),(1,11,12,13),(10,11,12,13)]
+    if hand[0] == JOKER:
+        return tuple(hand[i][1] for i in range(1, len(hand))) in [(1, 10, 11, 12), (1, 10, 11, 13), (1, 10, 12, 13), (1, 11, 12, 13), (10, 11, 12, 13)]
     else:
-        if hand[0][1]!=1:
+        if hand[0][1] != 1:
             return False
-        for i in range(1,len(hand)):
-            if hand[i][1]!=9+i:
+        for i in range(1, len(hand)):
+            if hand[i][1] != 9+i:
                 return False
         return True
+
 
 def isStraight(hand):
     if isRoyalStraight(hand):
         return True
-    if hand[0]==JOKER:
-        return tuple(hand[i][1]-hand[1][1] for i in range(2,len(hand))) in [(1,2,3),(1,2,4),(1,3,4),(2,3,4)]
+    if hand[0] == JOKER:
+        return tuple(hand[i][1]-hand[1][1] for i in range(2, len(hand))) in [(1, 2, 3), (1, 2, 4), (1, 3, 4), (2, 3, 4)]
     else:
-        for i in range(1,len(hand)):
-            if hand[i][1]!=hand[0][1]+i:
+        for i in range(1, len(hand)):
+            if hand[i][1] != hand[0][1]+i:
                 return False
         return True
 
+
 def isThreeOfAKind(hand):
-    if hand[0]==JOKER:
-        return hand[1][1]==hand[2][1] or hand[2][1]==hand[3][1] or hand[3][1]==hand[4][1]
+    if hand[0] == JOKER:
+        return hand[1][1] == hand[2][1] or hand[2][1] == hand[3][1] or hand[3][1] == hand[4][1]
     else:
-        return hand[0][1]==hand[1][1]==hand[2][1] or hand[1][1]==hand[2][1]==hand[3][1] or hand[2][1]==hand[3][1]==hand[4][1]
+        return hand[0][1] == hand[1][1] == hand[2][1] or hand[1][1] == hand[2][1] == hand[3][1] or hand[2][1] == hand[3][1] == hand[4][1]
+
 
 def isTwoPair(hand):
-    if hand[0]==JOKER:
-        return hand[1][1]==hand[2][1] or hand[2][1]==hand[3][1] or hand[3][1]==hand[4][1]
+    if hand[0] == JOKER:
+        return hand[1][1] == hand[2][1] or hand[2][1] == hand[3][1] or hand[3][1] == hand[4][1]
     else:
-        return hand[0][1]==hand[1][1] and hand[2][1]==hand[3][1]\
-            or hand[0][1]==hand[1][1] and hand[3][1]==hand[4][1]\
-            or hand[1][1]==hand[2][1] and hand[3][1]==hand[4][1]
+        return hand[0][1] == hand[1][1] and hand[2][1] == hand[3][1]\
+            or hand[0][1] == hand[1][1] and hand[3][1] == hand[4][1]\
+            or hand[1][1] == hand[2][1] and hand[3][1] == hand[4][1]
+
 
 def isOnePair(hand):
-    if hand[0]==JOKER:
+    if hand[0] == JOKER:
         return True
     else:
-        for i in range(1,len(hand)):
-            if hand[i-1][1]==hand[i][1]:
+        for i in range(1, len(hand)):
+            if hand[i-1][1] == hand[i][1]:
                 return True
         return False
+
 
 def deal(hand, k):
     global nPatterns
@@ -124,54 +136,56 @@ def deal(hand, k):
     global cntOnePair
     global cntNoPair
 
-    if len(hand)<5:
-        for i in range(k,len(CARDS)):
+    if len(hand) < 5:
+        for i in range(k, len(CARDS)):
             hand.append(CARDS[i])
-            deal(hand,i+1)
+            deal(hand, i+1)
             hand.pop()
     else:
-        for i in range(1,len(hand)):
-            assert(hand[i-1][1]<=hand[i][1])
-        
-        nPatterns+=1
+        for i in range(1, len(hand)):
+            assert (hand[i-1][1] <= hand[i][1])
+
+        nPatterns += 1
         if isFiveOfAKind(hand):
-            cntFiveOfAKind+=1
+            cntFiveOfAKind += 1
         elif isRoyalStraightFlush(hand):
-            cntRoyalStraightFlush+=1
+            cntRoyalStraightFlush += 1
         elif isStraightFlush(hand):
-            cntStraightFlush+=1
+            cntStraightFlush += 1
         elif isFourOfAKind(hand):
-            cntFourOfAKind+=1
+            cntFourOfAKind += 1
         elif isFullHouse(hand):
-            cntFullHouse+=1
+            cntFullHouse += 1
         elif isFlush(hand):
-            cntFlush+=1
+            cntFlush += 1
         elif isStraight(hand):
-            cntStraight+=1
+            cntStraight += 1
         elif isThreeOfAKind(hand):
-            cntThreeOfAKind+=1
+            cntThreeOfAKind += 1
         elif isTwoPair(hand):
-            cntTwoPair+=1
+            cntTwoPair += 1
         elif isOnePair(hand):
-            cntOnePair+=1
+            cntOnePair += 1
         else:
-            cntNoPair+=1 
+            cntNoPair += 1
+
 
 def output(str, cnt):
     print(f"{str:<21}:{cnt:>8}({100*cnt/nPatterns:10.6f}%)")
 
-if __name__=="__main__":
-    deal([],0)
-    output("Five of a Kind",cntFiveOfAKind)
-    output("Royal Straight Flush",cntRoyalStraightFlush)
-    output("Straight Flush",cntStraightFlush)
-    output("Four of a Kind",cntFourOfAKind)
-    output("Full House",cntFullHouse)
-    output("Flush",cntFlush)
-    output("Straight",cntStraight)
-    output("Three of a Kind",cntThreeOfAKind)
-    output("Two Pair",cntTwoPair)
-    output("One Pair",cntOnePair)
-    output("No Pair",cntNoPair)
+
+if __name__ == "__main__":
+    deal([], 0)
+    output("Five of a Kind", cntFiveOfAKind)
+    output("Royal Straight Flush", cntRoyalStraightFlush)
+    output("Straight Flush", cntStraightFlush)
+    output("Four of a Kind", cntFourOfAKind)
+    output("Full House", cntFullHouse)
+    output("Flush", cntFlush)
+    output("Straight", cntStraight)
+    output("Three of a Kind", cntThreeOfAKind)
+    output("Two Pair", cntTwoPair)
+    output("One Pair", cntOnePair)
+    output("No Pair", cntNoPair)
     print('-'*len(f"{str():<21}:{int():>8}({float():10.6f}%)"))
-    output("Total",nPatterns)
+    output("Total", nPatterns)
